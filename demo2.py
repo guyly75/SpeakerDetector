90e3f66402978957373d5ca69b59fcd5a80fb561
Guy Levy <guyabrpad@gmail.com>
2023-02-05 21:04:21 +0200

    updated

--- demo2.py
+++ demo2.py
@@ -2,12 +2,36 @@ from resemblyzer import preprocess_wav, VoiceEncoder
 from demo_utils import *
 from pathlib import Path
 from scipy.io.wavfile import write
-from datetime import datetime
-import os
+from datetime import datetime,timedelta
+import os 
+import pickle
+import librosa
+import math
+
+
+speaking_level=0.82
 
 def is_speaking(similarity_value):
-    return similarity_value>=0.80 if True else False
+    return similarity_value>=speaking_level if True else False
 
+def is_speaking_low(similarity_value):
+    return similarity_value>=(speaking_level*0.95) if True else False
+
+def new_picks(wav,wav_splits,speaker_values):
+    window_size=10
+    good_match={a:b for a,b in enumerate(speaker_values) if b>speaking_level}
+    last_position=0
+    for _,ind in enumerate(good_match):
+        if ind<=last_position: 
+            continue 
+        if np.mean(speaker_values[ind-window_size:ind+window_size])>=speaking_level*0.95:
+            for ind1 in range(ind+1,len(speaker_values)-window_size):
+                if np.mean(speaker_values[ind1-window_size:ind1+window_size])<0.7:
+                    yield wav_splits[ind].start,wav_splits[ind1].stop
+                    last_position=ind1
+                    break
+            #yield wav_splits[ind].start,wav_splits[-1].stop
+    return
 
 def speaker_chunks(wav,wav_splits,speaker_values):
     seconds_per_window=wav_splits[0].stop/16000
@@ -17,55 +41,170 @@ def speaker_chunks(wav,wav_splits,speaker_values):
         if is_speaker_speaking == is_speaking(value): # no change
             continue
         elif is_speaker_speaking: # stop speaking
-            if is_speaking(max(speaker_values[position:int(position+(seconds_per_window*10))])):
+            if is_speaking_low(max(speaker_values[position:int(position+(seconds_per_window*100))])):
                 continue
             else:
                 yield wav_splits[start_speak].start,wav_splits[position-1].stop
-        else: # starts to speak
+        elif is_speaking_low(max(speaker_values[position:int(position+(seconds_per_window*10))])): # starts to speak
             start_speak=position
         is_speaker_speaking=is_speaking(value)
     return
 
-
-print(f'starting: {datetime.now()}')
-
-## Get reference audios
-# Load the interview audio from disk
-# Source for the interview: https://www.youtube.com/watch?v=X2zqiX6yL3I
-
-input_filename=r'H:\My Drive\Speakers Project\Data\Audio Extraction\1293ID-Session-547662.wav'
-wav_fpath = Path(input_filename)
-#wav_fpath = Path(r'X2zqiX6yL3I.mp3')
-rubi_fpath= Path(r'H:\My Drive\Speakers Project\Data\Audio Extraction\speakers\rubi.wav')
-merav_fpath= Path(r'H:\My Drive\Speakers Project\Data\Audio Extraction\speakers\MeiravMichaeli.wav')
-wav = preprocess_wav(wav_fpath)
-rubi_wav=preprocess_wav(rubi_fpath)
-merav_wav=preprocess_wav(merav_fpath)
-
-speaker_names = ["Rubi Rivlin","Merav Michaeli"]
-speaker_wavs = [rubi_wav,merav_wav]
+def get_from_or_create_file(file_name,function_to_execute):
+    ## Read or Generate a file using the function_to_execute
+    my_file = Path(file_name)
+    if my_file.exists():
+        with open(my_file, "rb") as infile:
+            ret= pickle.load(infile)
+    else:
+        ret=function_to_execute()    
+        with open(my_file, "wb") as outfile:
+            pickle.dump(ret, outfile)
+    return ret
     
-## Compare speaker embeds to the continuous embedding of the interview
-# Derive a continuous embedding of the interview. We put a rate of 16, meaning that an 
-# embedding is generated every 0.0625 seconds. It is good to have a higher rate for speaker 
-# diarization, but it is not so useful for when you only need a summary embedding of the 
-# entire utterance. A rate of 2 would have been enough, but 16 is nice for the sake of  the 
-# demonstration. 
-# We'll exceptionally force to run this on CPU, because it uses a lot of RAM and most GPUs 
-# won't have enough. There's a speed drawback, but it remains reasonable.
-encoder = VoiceEncoder("cpu")
-print("Running the continuous embedding on cpu, this might take a while...")
-_, cont_embeds, wav_splits = encoder.embed_utterance(wav, return_partials=True, rate=3)
 
 
-# Get the continuous similarity for every speaker. It amounts to a dot product between the 
-# embedding of the speaker and the continuous embedding of the interview
-speaker_embeds = [encoder.embed_utterance(speaker_wav) for speaker_wav in speaker_wavs]
-similarity_dict = {name: cont_embeds @ speaker_embed for name, speaker_embed in 
-                   zip(speaker_names, speaker_embeds)}
-print(f'similaroty dictionary done: {datetime.now()}')        
-for speaker in speaker_names:
-    for counter,(start,stop) in enumerate(speaker_chunks(wav,wav_splits,similarity_dict[speaker])):
-        write(f'output\\{os.path.basename(input_filename)} - {speaker}-{start/16000:.1f}-{stop/16000:.1f}.wav',16000,np.array(wav[start:stop]))
+def embed_uttorence_chunks(input_filename,return_partials,rate):        
+    ## Compare speaker embeds to the continuous embedding of the interview
+    # Derive a continuous embedding of the interview. We put a rate of 16, meaning that an 
+    # embedding is generated every 0.0625 seconds. It is good to have a higher rate for speaker 
+    # diarization, but it is not so useful for when you only need a summary embedding of the 
+    # entire utterance. A rate of 2 would have been enough, but 16 is nice for the sake of  the 
+    # demonstration. 
+    # We'll exceptionally force to run this on CPU, because it uses a lot of RAM and most GPUs 
+    # won't have enough. There's a speed drawback, but it remains reasonable.
 
-print(f'done: {datetime.now()}')        
+    wav=get_from_or_create_file(input_filename+'XX.dat',lambda: preprocess_wav(Path(input_filename)))
+
+    encoder = VoiceEncoder("cpu")
+
+    folder=os.path.dirname(input_filename)
+    file_name=os.path.basename(input_filename)
+    files_list = list(Path(folder).glob(file_name+'-embed*'))
+    if len(files_list)>0:
+        checked_file=files_list[0].name
+        num_of_chunks=int(checked_file[checked_file.find('-embed')+6:checked_file.find('-embed')+7])
+    else:
+        num_of_chunks=1
+    chunk_number=0    
+    while True:
+        chunk_size=math.floor(len(wav)/num_of_chunks)        
+        chunk_start=chunk_size*chunk_number
+        chunk_end=chunk_start+chunk_size
+        try:            
+            ret_embed = get_from_or_create_file(input_filename+f'-embed{num_of_chunks}-{chunk_number}-rate{rate}.dat',\
+            lambda: encoder.embed_utterance(wav[chunk_start:chunk_end], return_partials, rate))
+
+            (embed,partial_embeds,wav_slices)=ret_embed
+            yield wav[chunk_start:chunk_end],partial_embeds,wav_slices
+
+        except RuntimeError:
+            num_of_chunks+=1
+            chunk_number=0
+            continue
+        chunk_number+=1
+        if chunk_number==num_of_chunks:
+            return
+
+
+def get_speakers_data():
+    # assign directory
+    encoder = VoiceEncoder("cpu")
+    speakers_folder = r'G:\My Drive\Speakers Project\Data\Audio Extraction\speakers'
+    files = Path(speakers_folder).glob('*.wav')
+    speakers_data={}
+    wav_data={}
+    for file in files:
+        is_preproccesed=False
+        if file.name[-11:]=='-prepro.wav':
+            continue
+        preproccessed_filename=str(file.resolve())+'-prepro.dat'
+        speaker_name=os.path.basename(preproccessed_filename)[5:-11]     
+#        prepro_file=Path(preproccessed_filename)
+#        if prepro_file.exists():
+#                is_preproccesed=True
+#                file=prepro_file
+#                speaker_name=os.path.basename(file)[5:-11]
+#        else:
+#            speaker_name=os.path.basename(file)[5:-4]
+        print(f"processing {file}")            
+        if speaker_name not in wav_data.keys():
+            speakers_data[speaker_name]=[]
+#            if is_preproccesed:
+            wav_data[speaker_name]=get_from_or_create_file(preproccessed_filename,lambda: preprocess_wav(file,trim_long_silences=True))
+#            else:
+#                wav_data[speaker_name]=preprocess_wav(file)
+#                write(preproccessed_filename,16000,wav_data[speaker_name])
+        else:
+            s=preprocess_wav(file,trim_long_silences=True)
+            print(f"bfore",len(s),type(s))
+            wav_data[speaker_name]+=s
+            print(f"after")            
+    print(f"generating embeds")
+    for speaker_name in wav_data.keys():
+        print(f"generating embeds for {speaker_name}")
+        speakers_data[speaker_name]=get_from_or_create_file(f'{speakers_folder}\\{speaker_name}_utterance.dat',lambda: encoder.embed_utterance(wav_data[speaker_name]))
+    return speakers_data
+
+
+def process_file(input_filename,speakers_data):
+    print(f'starting: {input_filename} - {datetime.now()}')
+    prm_infile = Path(input_filename)
+    if not prm_infile.exists():
+        return
+    ## Get reference audios
+    # Load the interview audio from disk
+    # Source for the interview: https://www.youtube.com/watch?v=X2zqiX6yL3I
+
+    # dat_file=input_filename+'XX.dat';
+    # wav_fpath = Path(input_filename)
+    # my_file = Path(dat_file)
+    # if my_file.exists():
+    #     with open(dat_file, "rb") as infile:
+    #         wav= pickle.load(infile)
+    # else:
+    #     wav=preprocess_wav(wav_fpath)    
+    #     with open(input_filename+'XX.dat', "wb") as outfile:
+    #         pickle.dump(wav, outfile);
+    speaker_names = speakers_data.keys()
+    speaker_embeds = speakers_data.values()
+        
+    print("Running the continuous embedding on cpu, this might take a while...")
+
+    for part,(processed_wav, cont_embeds, wav_splits) in enumerate(embed_uttorence_chunks(input_filename,True,2)):
+
+        # Get the continuous similarity for every speaker. It amounts to a dot product between the 
+        # embedding of the speaker and the continuous embedding of the interview
+        #speaker_embeds = [encoder.embed_utterance(speaker_wav) for speaker_wav in speaker_wavs]
+        similarity_dict = {name: cont_embeds @ speaker_embed for name, speaker_embed in 
+                        zip(speaker_names, speaker_embeds)}
+        print(f'\n*********  S I M I L A R I T Y        V A L U E S for {os.path.basename(input_filename)}***************')
+        for speaker in speaker_names:    
+            print(f'{speaker[:-4]}: max similarity: {max(similarity_dict[speaker])}')
+            if speaker[-7:]=="-prepro":
+                speaker=speaker[:-7]    
+            for counter,(start,stop) in enumerate(new_picks(processed_wav,wav_splits,similarity_dict[speaker])):
+                folder=f'G:\\My Drive\\Speakers Project\\processed\\{speaker}'
+                if not Path(folder).exists():
+                    os.mkdir(folder)
+                write(f'{folder}\\{os.path.basename(input_filename)}-{part}-{speaker}-{(datetime.min+timedelta(seconds=start/16000)):%H`%M`%S}-{(datetime.min+timedelta(seconds=stop/16000)):%H`%M`%S}.wav',16000,np.array(processed_wav[start:stop]))
+
+        print(f'*********  D O N E: {os.path.basename(input_filename)}  ***************\n')                     
+       
+    print(f'complete: {input_filename} - {datetime.now()}')     
+
+#sessions_folder = r'G:\My Drive\Speakers Project\Data\Audio Extraction'
+#files = Path(sessions_folder).glob('*.wav')
+#for file in files:
+    #process_file(file)
+def main():
+    speakers_data=get_speakers_data()
+
+    sessions_folder = r'G:\My Drive\Speakers Project\Data\Audio Extraction\speakers'
+    files = Path(sessions_folder).glob('*.wav')
+    for file in files:
+        process_file(file.name,speakers_data)
+
+    print(f'done: {datetime.now()}')        
+
+main()
